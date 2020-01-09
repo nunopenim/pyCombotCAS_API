b5c03d8b2d4c16125f21e5426ce6735f44fffb96
Nuno Penim <nuno.penim@gmail.com>
2020-01-09 17:17:53 +0000

    moved dev api to master as a separate file

---
+++ cas_api_dev.py
@@ -0,0 +1,45 @@
+import urllib.request as url
+import json
+import datetime
+
+DEVUSER = "" #user ID that is banned for testing
+VERSION = "1.3.1 - DEV"
+CAS_QUERY_URL = "https://combot.org/api/cas/check?user_id="
+DL_DIR = "./csvExports"
+
+def get_user_data(user_id):
+    with url.urlopen(CAS_QUERY_URL + str(user_id)) as userdata_raw:
+        userdata = json.loads(userdata_raw.read().decode())
+        return userdata
+
+def isbanned(userdata):
+    return userdata['ok']
+
+def banchecker(user_id):
+    if(str(user_id) == DEVUSER):
+        return True
+    return isbanned(get_user_data(user_id))
+
+def vercheck() -> str:
+    return str(VERSION)
+
+def offenses(user_id):
+    if str(user_id) == DEVUSER:
+        return "TEST USER"
+    userdata = get_user_data(user_id)
+    try:
+        offenses = userdata['result']['offenses']
+        return str(offenses) 
+    except:
+        return None
+    
+def timeadded(user_id):
+    if str(user_id) == DEVUSER:
+        return "TEST USER - NEVER"
+    userdata = get_user_data(user_id)
+    try:
+        timeEp = userdata['result']['time_added']
+        timeHuman = datetime.datetime.utcfromtimestamp(timeEp).strftime('%H:%M:%S, %d-%m-%Y')
+        return timeHuman
+    except:
+        return None
